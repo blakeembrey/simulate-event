a19440d465f068d07c4194a48a6bd025f03f23a3
Steven Silvester <steven.silvester@ieee.org>
2016-06-13 10:16:50 -0500

    Clean up handling of other events

---
+++ test/events.js
@@ -0,0 +1,32 @@
+/* global describe, it, simulateEvent, sinon, expect, beforeEach, afterEach */
+
+describe('Events', function () {
+  var eventNames = 'abort change open storage loadend popstate transitionend '
+  eventNames += 'compositionend pagehide'
+  var events = eventNames.split(' ')
+  var fixture
+
+  beforeEach(function () {
+    fixture = document.createElement('div')
+    document.body.appendChild(fixture)
+  })
+
+  afterEach(function () {
+    fixture.parentNode.removeChild(fixture)
+    fixture = null
+  })
+
+  events.forEach(function (eventName) {
+    it('should trigger ' + eventName, function () {
+      var spy = sinon.spy()
+
+      fixture.addEventListener(eventName, spy)
+      simulateEvent.simulate(fixture, eventName)
+
+      expect(spy).to.have.been.calledOnce
+
+      var evt = simulateEvent.generate(eventName)
+      expect(evt.type).to.equal(eventName)
+    })
+  })
+})
